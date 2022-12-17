e96adc9db34d8e7a480dde54bbc5009d70a97c24
ldied <lennart.diedrichsen@protofy.com>
2022-12-17 22:23:08 +0100

    feat: add ts to devDeps and activate tailwind purge (hotreload of .ts)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./dist/*.html", "./dist/*.js"],
+  mode: "jit",
+  purge: ["./dist/*.html", "./src/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
